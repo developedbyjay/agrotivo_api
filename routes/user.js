c9ddc136e9de0a77eec9275c52607b6f77d0a3d7
develop_ed <72957022+developedbyjay@users.noreply.github.com>
2022-08-29 11:48:43 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,24 +5,6 @@ const { auth, admin } = require("../middleware/validation");
 const router = require("express").Router();
 
 //UPDATE
-router.put("/:id", auth, async (req, res) => {
-  if (req.body.password) {
-    req.body.password = await bcrypt.hash(req.body.password, 8).toString();
-  }
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedUser);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.put("/user/:id", auth, async (req, res) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(
